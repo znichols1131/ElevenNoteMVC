0cc48c252b2121d8ef3f2a165d78993c559eafdb
Zach Nichols <znichols1131@gmail.com>
2022-01-07 18:24:49 -0500

    Added Note data model and registered first user

---
+++ ElevenNote.Data/Note.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Data
+{
+    public class Note
+    {
+        [Key]
+        public int NoteId { get; set; }
+
+        [Required]
+        public Guid guid { get; set; }
+
+        [Required]
+        [Display(Name = "Title")]
+        public string Title { get; set; }
+
+        [Required]
+        [Display(Name = "Content")]
+        public string Content { get; set; }
+
+        [Required]
+        public DateTimeOffset dateTimeOffset { get; set; }
+        
+        public DateTimeOffset? ModifiedUtc { get; set; }
+    }
+}
