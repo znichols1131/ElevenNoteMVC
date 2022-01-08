0d924a3060720e84008097165f72a912ef6a8c6c
Zach Nichols <znichols1131@gmail.com>
2022-01-07 19:13:52 -0500

    Added successful save feedback via tempdata

--- ElevenNote.WebMVC/Controllers/NoteController.cs
+++ ElevenNote.WebMVC/Controllers/NoteController.cs
@@ -32,17 +32,26 @@ namespace ElevenNote.WebMVC.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Create(NoteCreate model)
         {
-            if(!ModelState.IsValid)
+            if (!ModelState.IsValid) return View(model);
+            
+            var service = CreateNoteService();
+
+            if (service.CreateNote(model))
             {
-                return View(model);
+                TempData["SaveResult"] = "Your note was created.";
+                return RedirectToAction("Index");
             }
 
+            ModelState.AddModelError("", "Note could not be created.");
+
+            return View(model);
+        }
+
+        private NoteService CreateNoteService()
+        {
             var userId = Guid.Parse(User.Identity.GetUserId());
             var service = new NoteService(userId);
-
-            service.CreateNote(model);
-
-            return RedirectToAction("Index");
+            return service;
         }
     }
 }
